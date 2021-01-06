5d9b8741b71c88f0e7ab68d3efd2be29fa80d364
Shubhranshu Mishra <shubymishra20@gmail.com>
2021-01-05 21:50:06 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # RedditNews2
 A website that uses Django + React to track the sentiment of events listed on reddit news
+
+
+
+# TODO:
+
+Establish structure 
+
+
+
+
---
+++ redditnews/reddit/util.py
@@ -0,0 +1,35 @@
+import requests
+
+
+
+ID = ""
+SECRET = ""
+
+
+
+
+
+BASE = "https://reddit.com/api/v1/me"
+
+
+
+def fetch_events():
+    client_auth = 
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    fetch_events()
+
+
+
+
+
