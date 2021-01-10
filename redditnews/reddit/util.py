637edf4fa0eb298f93cf8dd23c9a378732ff7938
Shubhranshu Mishra <shubymishra20@gmail.com>
2021-01-10 16:45:42 -0500

    have a specific return format now

--- redditnews/reddit/util.py
+++ redditnews/reddit/util.py
@@ -1,5 +1,5 @@
-import requests
 import json
+from requests import post, get, auth
 
 AUTH = "https://www.reddit.com/api/v1/access_token"
 API = "https://oauth.reddit.com"
@@ -7,21 +7,27 @@ USER_AGENT = "Reddit News JS by BambooSlayerz"
 CREDS = json.loads(open("creds.json").read())
 
 def get_token():
-    client_auth = requests.auth.HTTPBasicAuth(CREDS['id'], CREDS['secret'])
-    headers = {"User-Agent":USER_AGENT}
-    post = {'grant_type':'client_credentials'}
-    return requests.post(AUTH, auth=client_auth, data=post, headers=headers).json()["access_token"]
+    client_auth = auth.HTTPBasicAuth(CREDS['id'], CREDS['secret'])
+    headers = {"User-Agent": USER_AGENT}
+    data = {'grant_type':'client_credentials'}
+    return post(AUTH, auth=client_auth, data=data, headers=headers).json()["access_token"]
 
-
-def get(endpoint, token):
+def get_events(endpoint, token):
+    ret = {}
     headers = {"Authorization": "bearer " + token,"User-Agent":USER_AGENT} 
-    return requests.get(API + endpoint, headers=headers).json()
+    data = get(API + endpoint, headers=headers).json()
+    for event in data['data']['children']:
+        event = event['data']
+            
+        ret[event['title']] = {
+            "ups" : event['ups'],
+            "downs" : event['downs'],
+            "upvote_ratio" : event['upvote_ratio'],
+            "score" : None, #TODO: Potentially make request here
+            "created" : event['created_utc'] #unix time stamp
+        }
 
+    return ret
 
 if __name__ == "__main__":
-    events = get("/r/news/?limit=100", get_token())
-    for event in events['data']['children']:
-        print(event['data']['title'])
-
-
-
+    events = get_events("/r/news/?limit=100", get_token())
